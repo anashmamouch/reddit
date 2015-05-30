4faf21a73bad33d84797fca027894a28e36e9839
benzinoanas <benzinoanas@gmail.com>
2015-05-30 14:10:32 +0100

    modified migrations files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530114905) do
+ActiveRecord::Schema.define(version: 20150530130740) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150530114905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.text     "content"
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id"
@@ -36,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150530114905) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
